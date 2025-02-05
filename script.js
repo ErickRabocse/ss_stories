addbffec19949d203f5bbc6753b0e055486be46a
Erick Chavez <erickchavezescobar@gmail.com>
2025-02-05 13:39:52 -0600

    Stories feedback component. Need: verification "hashtag used & min length + visual indicator"

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const textareaEl = document.querySelector('.form__textarea')
 const counterEl = document.querySelector('.counter')
+const storiesFeedEl = document.querySelector('.stories')
+const formEl = document.querySelector('.form')
 //declare max amount of letters
 const MAX_CHARS = 500
+// Global variables
 
+// INPUT COMPONENT
 textareaEl.addEventListener('input', (e) => {
   // number of characters typed in the textarea
   const characters = e.target.value.length
@@ -11,3 +15,19 @@ textareaEl.addEventListener('input', (e) => {
   // updating counter
   counterEl.textContent = charsLeft
 })
+// STORIES FEEDBACK COMPONENT
+
+// fn used to render list item
+const handleStoryPost = (e) => {
+  e.preventDefault()
+  const storyEl = textareaEl.value
+  // HTML feed to be posted in the list
+  const storyListEl = `
+    <li class="storyElList">
+      <p>${storyEl}</p>
+    </li>
+  `
+  storiesFeedEl.insertAdjacentHTML('beforeend', storyListEl)
+}
+
+formEl.addEventListener('submit', handleStoryPost)
